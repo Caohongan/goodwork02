c9e735d447937cd198e872271153fe6d3b86e38f
Ân Cao <caohongan987@gmail.com>
2021-01-01 23:31:35 +0700

    first commit

---
+++ resources/assets/js/components/partials/navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="bg-white flex flex-row justify-between h-12 md:px-4 shadow top-0 z-50">
+    <a class="flex items-center text-teal-500 text-2xl font-bold md:font-normal no-underline h-12 w-20 md:w-32" :href="url.site">
+      <img :src="generateUrl('logos/truong_dai_hoc_lac_hong_logo.svg')" alt="" class="w-full h-10 block">
+      <span class="text-gray-700 font-bold md:block">Workspace</span>
+    </a>
+
+    <div class="md:flex">
+      <div class="flex flex-row border-l h-full">
+        <message-dropdown></message-dropdown>
+        <profile-dropdown></profile-dropdown>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+  import notificationDropdown from './notificationDropdown'
+  import messageDropdown from './messageDropdown'
+  import profileDropdown from './profileDropdown'
+  export default {
+    components: {notificationDropdown, messageDropdown, profileDropdown},
+    data: () => ({
+      token: Laravel.csrfToken,
+      url: navUrl
+    })
+  }
+</script>
---
+++ resources/assets/js/components/users/settings.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto mt-8 px-4 md:px-0 w-full md:max-w-2xl lg:max-w-4xl xl:max-w-6xl text-gray-800">
+  
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
